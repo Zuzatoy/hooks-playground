3fee49a3cfeb4824f67c7aeaf0e140dbccc049e0
T826631 <zuzatoy@gmail.com>
2019-04-19 02:02:37 +1200

    transpiled code to Class/Function Counter

--- src/Todo/CounterClass.js
+++ src/Todo/CounterClass.js
@@ -34,3 +34,71 @@ class CounterClass extends Component {
 }
 
 export default CounterClass;
+
+// var CounterClass =
+// /*#__PURE__*/
+// function (_Component) {
+//   _inherits(CounterClass, _Component);
+
+//   function CounterClass() {
+//     var _getPrototypeOf2;
+
+//     var _this;
+
+//     _classCallCheck(this, CounterClass);
+
+//     for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+//       args[_key] = arguments[_key];
+//     }
+
+//     _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(CounterClass)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+//     _defineProperty(_assertThisInitialized(_this), "state", {
+//       counter: 0
+//     });
+
+//     _defineProperty(_assertThisInitialized(_this), "incrementCounter", function () {
+//       _this.setState(function (state) {
+//         return {
+//           counter: state.counter + 1
+//         };
+//       });
+//     });
+
+//     _defineProperty(_assertThisInitialized(_this), "decrementCounter", function () {
+//       _this.setState(function (state) {
+//         return {
+//           counter: state.counter - 1
+//         };
+//       });
+//     });
+
+//     _defineProperty(_assertThisInitialized(_this), "resetCounter", function () {
+//       _this.setState(function (state) {
+//         return {
+//           counter: 0
+//         };
+//       });
+//     });
+
+//     return _this;
+//   }
+
+//   _createClass(CounterClass, [{
+//     key: "render",
+//     value: function render() {
+//       return React.createElement("div", null, React.createElement("h1", null, this.state.counter), React.createElement("button", {
+//         onClick: this.incrementCounter
+//       }, "+"), React.createElement("button", {
+//         onClick: this.decrementCounter
+//       }, "-"), React.createElement("button", {
+//         onClick: this.resetCounter
+//       }, "reset"));
+//     }
+//   }]);
+
+//   return CounterClass;
+// }(Component);
+
+// var _default = CounterClass;
+// exports.default = _default;
--- src/Todo/CounterFunction.js
+++ src/Todo/CounterFunction.js
@@ -12,3 +12,24 @@ export default function CounterFunction () {
         </div>
     )
 }
+
+// function CounterFunction() {
+//   var _useState = useState(0),
+//       _useState2 = _slicedToArray(_useState, 2),
+//       counter = _useState2[0],
+//       setCounter = _useState2[1];
+
+//   return React.createElement("div", null, React.createElement("p", null, counter), React.createElement("button", {
+//     onClick: function onClick() {
+//       return setCounter(counter + 1);
+//     }
+//   }, "+"), React.createElement("button", {
+//     onClick: function onClick() {
+//       return setCounter(counter - 1);
+//     }
+//   }, "-"), React.createElement("button", {
+//     onClick: function onClick() {
+//       return setCounter(0);
+//     }
+//   }, "reset"));
+// }
